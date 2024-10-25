f917ada0f0422721724a1cb738407ffec23abe8c
Jose de Lima <delimajm@gmail.com>
2024-10-24 22:08:07 -0500

    Data types and variables

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int sudentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'J';
+        char studentLastInitial = 'D';
+        boolean hasPerfectAttendance = true;
+
+
+        System.out.println(sudentAge);
+        System.out.println(studentGPA);
+        System.out.println("" + studentFirstInitial + studentLastInitial);
+        System.out.println(hasPerfectAttendance);
     }
 }
