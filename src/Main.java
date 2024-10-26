b40d64dbda5255f1e3f44743bf89f8775274217e
Jose de Lima <delimajm@gmail.com>
2024-10-26 17:09:27 -0500

    Defining functions in java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
+    public static void annouceDeveloperTeaTime() {
+        System.out.println("Waiting for developer team time...");
+        System.out.println("Type in a random word and Enter to start developer tea time.");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's developer tea time!");
+    }
     public static void main(String[] args) {
-
+        annouceDeveloperTeaTime();
     }
 
 }
