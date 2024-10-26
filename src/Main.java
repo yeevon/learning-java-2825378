9e29ea57b23ad0e0a41a90764c86e1308c3b861b
Jose de Lima <delimajm@gmail.com>
2024-10-26 18:15:32 -0500

    Caling Functions

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
 
-    public static void calculateTotalMealPrice() {
+    public static double calculateTotalMealPrice(
+            double tipRate,
+            double listedMealPrice,
+            double taxRate
+    ) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        return listedMealPrice + tip + tax;
     }
 
     public static void main(String[] args) {
+        double groupTotalMealPrice = calculateTotalMealPrice(.18, 22.2, .08);
+        System.out.println(groupTotalMealPrice);
     }
 
 }
