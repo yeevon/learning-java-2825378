dd3fc858a51d08dd3ae8bb984de24e2221cdeb9a
Jose de Lima <delimajm@gmail.com>
2024-10-26 08:06:55 -0500

    while loops in java

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean isOnRepeat = true;
+
+        while (isOnRepeat) {
+            System.out.println("Playing current song");
+            System.out.println("Would you like to take this song off of repeate? If so, answer yes");
+            String userInput = input.next();
+
+            isOnRepeat = !userInput.equals("yes");
+        }
+        System.out.println("Playing next song");
     }
 }
